6c326a33e3f6f0d496bbdc20c6b9cc5d729339ac
Start Agencia Digital <start.agenciadigitaloficial@gmail.com>
2026-04-21 00:20:43 -0300

    fix: add missing Link import in ProductCatalogClient

--- components/catalog/ProductCatalogClient.tsx
+++ components/catalog/ProductCatalogClient.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useEffect, useMemo, useRef, useState } from "react";
+import Link from "next/link";
 import { trackAnalyticsEvent } from "@/lib/analytics";
 import { motion, AnimatePresence, LayoutGroup } from "framer-motion";
 import { 
