ace4f56f5fb8320d69d1fe630f87c84107b07bcc
Start Agencia Digital <start.agenciadigitaloficial@gmail.com>
2026-06-30 21:01:33 -0300

    UI: Alteração do layout dos catálogos para cards empilhados verticalmente

--- app/dashboard/catalogo/gerenciador/CatalogManagerClient.tsx
+++ app/dashboard/catalogo/gerenciador/CatalogManagerClient.tsx
@@ -54,7 +54,7 @@ export default function CatalogManagerClient({
   };
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+    <div className="flex flex-col gap-6 max-w-4xl">
       <AnimatePresence>
         {localCatalogs.map((catalog) => (
           <motion.div
