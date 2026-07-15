61880f6daa3f0a8eb3742bf0e9cf3d660bd0908c
Start Agencia Digital <start.agenciadigitaloficial@gmail.com>
2026-07-14 22:55:35 -0300

    fix: corrigir svg do icone do tiktok

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -933,7 +933,7 @@ export default async function Page(props: PageProps) {
                 className="hover:scale-110 hover:bg-zinc-100 dark:hover:bg-zinc-800"
               >
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                  <path d="M9 12a4 4 0 1 0 4 4V4a5 5 0 0 0 5 5v3a3 3 0 0 1-3-3v11a7 7 0 1 1-7-7z"/>
+                  <path d="M21 8v2a12.8 12.8 0 0 1 -7 -2v10a6 6 0 1 1 -6 -6v2a4 4 0 1 0 4 4v-16h2a8 8 0 0 0 7 6z" />
                 </svg>
               </a>
             )}
